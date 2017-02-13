3e3f91bd0d2040a01cc196363dd9ef72a55627e9
Andrei Petrov <gelu@tut.by>
2017-02-13 18:46:58 +0300

    Flake8 failure.

--- sample.py
+++ sample.py
@@ -3,4 +3,10 @@ import math
 
 NUMBER = 16
 
-print('Square root of {} is {}'.format(NUMBER, math.sqrt(16)))
+def main():
+    DENOMINATOR = 4
+    print('Square root of {} is {}'.format(NUMBER, math.sqrt(NUMBER)))
+
+
+if __name__ == '__main__':
+    main()
